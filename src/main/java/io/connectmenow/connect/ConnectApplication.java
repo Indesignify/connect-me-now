f7c9563e089e58cd029e77ba68c23e9d4e4f7787
kolombina <kolombina0711@gmail.com>
2018-03-19 19:57:27 +0300

    Test server

---
+++ src/main/java/io/connectmenow/connect/ConnectApplication.java
@@ -0,0 +1,12 @@
+package io.connectmenow.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectApplication.class, args);
+	}
+}
