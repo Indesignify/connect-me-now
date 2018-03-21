2d982d963e212f499f2c94eb3269c24debedbb75
Indesignify <Indesignify@gmail.com>
2018-03-21 09:52:16 +0300

    Resolving issue #1. DB entities with Hibernate annotations generated.

---
+++ src/main/java/io/connectmenow/connect/UsersEntity.java
@@ -0,0 +1,131 @@
+package io.connectmenow.connect;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users", schema = "public", catalog = "postgres")
+public class UsersEntity {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String nickname;
+    private String password;
+    private String email;
+    private Timestamp registrationDate;
+    private Timestamp lastOnline;
+    private String status;
+
+    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "first_name", nullable = false, length = 80)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "last_name", nullable = false, length = 80)
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Basic
+    @Column(name = "nickname", nullable = false, length = 80)
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 80)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = false, length = 255)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "registration_date", nullable = true)
+    public Timestamp getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Timestamp registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Basic
+    @Column(name = "last_online", nullable = true)
+    public Timestamp getLastOnline() {
+        return lastOnline;
+    }
+
+    public void setLastOnline(Timestamp lastOnline) {
+        this.lastOnline = lastOnline;
+    }
+
+    @Basic
+    @Column(name = "status", nullable = true)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsersEntity that = (UsersEntity) o;
+        return id == that.id &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(nickname, that.nickname) &&
+                Objects.equals(password, that.password) &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(registrationDate, that.registrationDate) &&
+                Objects.equals(lastOnline, that.lastOnline) &&
+                Objects.equals(status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, firstName, lastName, nickname, password, email, registrationDate, lastOnline, status);
+    }
+}
