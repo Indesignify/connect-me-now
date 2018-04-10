837686ee5a1cd1b344ab5bda33f0fd4c60a00005
Indesignify <Indesignify@gmail.com>
2018-04-11 00:25:28 +0300

    Configs updated a little bit. Swagger now properly shows API implemented.

--- src/main/java/io/connectmenow/connect/swagger/SwaggerConfig.java
+++ src/main/java/io/connectmenow/connect/swagger/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.basePackage("com.connectmenow.connect.controller"))
+        .apis(RequestHandlerSelectors.any())
         .paths(PathSelectors.any())
         .build()
         .apiInfo(apiInfo());
